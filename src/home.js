c158bf0549be4bd92c92e994b24b1f24d978114e
CeeJayyy007 <jamesjamil007@gmail.com>
2021-07-14 22:26:25 +0100

    React practice

--- src/home.js
+++ src/home.js
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
+
 const Home = () => {
 
+    const [blogs, setBlogs] = useState(
+        [{title: "My new website", body: "Body of the new blog", author: "Jack Sparrow", id: 1},
+        {title: "My new blog news", body: "Body of the new blog", author: "Jack Bauer", id: 2},
+        {title: "My new FAQ", body: "Body of the new blog", author: "Jack Black", id: 3},   
+    ]);
     const handleClick = () => {
-        console.log('Hello');
+        
     }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                </div>
+
+            ))}
+            
+            
+            
             <button onClick={handleClick}>Click me</button>
         </div>
      );
